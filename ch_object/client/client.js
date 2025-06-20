bbeb7ac0675e86480d6fb87653b8bee84ca47fff
igorgp06 <igorgoncalvesp20@gmail.com>
2025-06-20 00:05:55 -0300

    Melhorias da otimização para os script de criação dos objetos

--- ch_object/client/client.js
+++ ch_object/client/client.js
@@ -1,9 +1,14 @@
 RegisterCommand('createObj', async () => {
 
     const model = 'prop_conc_blocks01c'
-    await carregarModelo(model)
+    await carregarModelo(model);
 
-    const [x, y, z] = GetEntityCoords(PlayerPedId())
+    const [x, y, z] = GetEntityCoords(PlayerPedId());
 
-    CreateObject(model, x, y, z - 1, false, false, false)
+    const pallet = CreateObject(model, x, y, z - 1, false, false, false);
+
+    SetObjectAsNoLongerNeeded(pallet);
+    SetModelAsNoLongerNeeded(model);
 });
+
+RegisterKeyMapping('createObj', 'Criar um palete de pedras', 'keyboard', 'H')
